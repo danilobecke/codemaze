9e87d9144780e7532d2d921bdd31d003bc7cd44c
Danilo Becke <danilobecke@gmail.com>
2023-06-12 16:27:19 -0300

    Code style

--- code/repository/dto/manager.py
+++ code/repository/dto/manager.py
@@ -3,10 +3,10 @@ from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 class ManagerDTO(UserDTO):
-    __tablename__ = "manager"
+    __tablename__ = 'manager'
 
     id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    groups = relationship("GroupDTO", back_populates="manager", cascade="all, delete")
+    groups = relationship('GroupDTO', back_populates='manager', cascade='all, delete')
 
     __mapper_args__ = {
         'polymorphic_identity':'manager'
