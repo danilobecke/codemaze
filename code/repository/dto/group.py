ada0a96cbf4cd58083cdf4b0211e5f35a5f30f98
Danilo Becke <danilobecke@gmail.com>
2023-06-12 19:45:23 -0300

    Add active on Group

--- code/repository/dto/group.py
+++ code/repository/dto/group.py
@@ -1,5 +1,5 @@
 from repository.base import Base
-from sqlalchemy import Column, Integer, Sequence, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, Sequence, String, ForeignKey, DateTime, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from repository.dto.student_group import student_group
@@ -8,6 +8,7 @@ class GroupDTO(Base):
     __tablename__ = 'group'
 
     id = Column(Integer, Sequence('sq_group_pk'), primary_key=True, autoincrement=True)
+    active = Column(Boolean, nullable=False, default=True)
     name = Column(String, nullable=False)
     code = Column(String(6), nullable=False)
     manager_id = Column(Integer, ForeignKey('manager.id'), nullable=False)
