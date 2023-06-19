2449a2aa0c455ef178153152e4059ec803108956
Danilo Becke <danilobecke@gmail.com>
2023-06-19 17:26:25 -0300

    Reset tests database before running tests

--- code/app.py
+++ code/app.py
@@ -4,10 +4,10 @@ from repository.database import Database
 from services.session_service import SessionService
 import os
 
-def __init_app(db_string: str):
+def __init_app(db_string: str, resetting_db: bool = False):
     key = __get_env('CODEMAZE_KEY')
     app = Flask(__name__)
-    Database.initialize(db_string)
+    Database.initialize(db_string, resetting_db)
     SessionService.initialize(key)
     Router(app).create_routes()
     return app
@@ -25,7 +25,7 @@ def run_as_debug():
 
 def run_as_test():
     db_string = __get_env('TEST_DB_STRING')
-    app = __init_app(db_string)
+    app = __init_app(db_string, resetting_db=True)
     return app.test_client()
 
 if __name__ == '__main__':
--- code/repository/database.py
+++ code/repository/database.py
@@ -7,17 +7,19 @@ import repository.table_builder
 class Database:
     shared: Self | None = None
 
-    def __init__(self, db_string: str):
-        connection = self.__create_connection(db_string)
+    def __init__(self, db_string: str, resetting: bool):
+        connection = self.__create_connection(db_string, resetting)
         self.session = sessionmaker(bind=connection)()
     
-    def __create_connection(self, db_string: str):
+    def __create_connection(self, db_string: str, resetting: bool):
         connection = create_engine(db_string)
+        if resetting:
+            Base.metadata.drop_all(bind=connection)
         Base.metadata.create_all(connection)
         return connection
 
     @staticmethod
-    def initialize(db_string: str):
+    def initialize(db_string: str, resetting: bool = False):
         if Database.shared is not None:
             return
-        Database.shared = Database(db_string)
+        Database.shared = Database(db_string, resetting)
