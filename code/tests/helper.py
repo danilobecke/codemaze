61b40a54b7c8ece58afd74a74874d1c3caf4aac6
Danilo Becke <danilobecke@gmail.com>
2023-06-19 15:54:28 -0300

    Code style

--- code/tests/helper.py
+++ code/tests/helper.py
@@ -10,21 +10,21 @@ HTTPResponse = tuple[StatusCode, any]
 
 # HTTP METHODS
 
-def headers(token: str | None) -> dict[str, str] | None:
+def __headers(token: str | None) -> dict[str, str] | None:
     if not token:
         return None
     return {"Authorization": f"Bearer {token}"}
 
 def get(path: str, token: str | None = None) -> HTTPResponse:
-    response = __app.get(path, headers=headers(token))
+    response = __app.get(path, headers=__headers(token))
     return (response.status_code, json.loads(response.data.decode('utf-8')))
 
 def post(path: str, payload: dict, token: str | None = None) -> HTTPResponse:
-    response = __app.post(path, data=json.dumps(payload), content_type=__CONTENT_TYPE_JSON, headers=headers(token))
+    response = __app.post(path, data=json.dumps(payload), content_type=__CONTENT_TYPE_JSON, headers=__headers(token))
     return (response.status_code, json.loads(response.data.decode('utf-8')))
 
 def patch(path: str, payload: dict, token: str | None = None) -> HTTPResponse:
-    response = __app.patch(path, data=json.dumps(payload), content_type=__CONTENT_TYPE_JSON, headers=headers(token))
+    response = __app.patch(path, data=json.dumps(payload), content_type=__CONTENT_TYPE_JSON, headers=__headers(token))
     return (response.status_code, json.loads(response.data.decode('utf-8')))
 
 # DATABSE HELPERS
