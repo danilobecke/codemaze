1dd400237f91192706225bc09925a9a876be6e8f
Danilo Becke <danilobecke@gmail.com>
2023-08-04 11:19:11 -0300

    Update GroupRepository to use join requests

--- code/repository/group_repository.py
+++ code/repository/group_repository.py
@@ -1,4 +1,4 @@
-from sqlalchemy import insert, select, update, delete
+from sqlalchemy import insert, select, update, delete, and_
 from sqlalchemy.exc import IntegrityError
 
 from helpers.exceptions import NotFound, Internal_UniqueViolation, ServerError
@@ -14,7 +14,7 @@ class GroupRepository(AbstractRepository[GroupDTO]):
         super().__init__(GroupDTO)
 
     def add_join_request(self, code: str, student_id: int) -> None:
-        group = self._session.query(GroupDTO).filter_by(code=code, active=True).first()
+        group = self._session.scalars(select(GroupDTO).where(and_(GroupDTO.code == code, GroupDTO.active == True))).first()
         if not group:
             raise NotFound()
         try:
@@ -29,14 +29,17 @@ class GroupRepository(AbstractRepository[GroupDTO]):
             raise ServerError() from e
 
     def get_students_with_join_request(self, group_id: int, approved: bool = False) -> list[StudentDTO]:
-        stm = select(student_group.columns).where(student_group.columns["group_id"] == group_id).where(student_group.columns["approved"] == approved)
-        join_requests = self._session.execute(stm)
-        students = list(map(lambda row: row.student_id, join_requests.all()))
-        result: list[StudentDTO] = self._session.query(StudentDTO).filter(StudentDTO.id.in_(students)).order_by(StudentDTO.created_at).all()
-        return result
+        stm = select(StudentDTO)\
+            .join(student_group, StudentDTO.id == student_group.columns['student_id'])\
+            .where(and_(student_group.columns['group_id'] == group_id, student_group.columns['approved'] == approved))\
+            .order_by(student_group.columns['updated_at'])
+        return list(self._session.scalars(stm).all())
 
     def __find_opened_join_request(self, group_id: int, student_id: int) -> None:
-        stm = select(student_group.columns).where(student_group.columns["group_id"] == group_id).where(student_group.columns["student_id"] == student_id).where(student_group.columns["approved"] == False)
+        stm = select(student_group)\
+            .where(student_group.columns['group_id'] == group_id)\
+            .where(student_group.columns['student_id'] == student_id)\
+            .where(student_group.columns['approved'] == False)
         result = self._session.execute(stm)
         if len(result.all()) == 0:
             raise NotFound()
@@ -44,7 +47,11 @@ class GroupRepository(AbstractRepository[GroupDTO]):
     def approve_join_request(self, group_id: int, student_id: int) -> None:
         self.__find_opened_join_request(group_id, student_id)
         try:
-            stm = update(student_group).where(student_group.columns["group_id"] == group_id).where(student_group.columns["student_id"] == student_id).where(student_group.columns["approved"] == False).values(approved=True)
+            stm = update(student_group)\
+                .where(student_group.columns['group_id'] == group_id)\
+                .where(student_group.columns['student_id'] == student_id)\
+                .where(student_group.columns['approved'] == False)\
+                .values(approved=True)
             self._session.execute(stm)
             self._session.commit()
         except Exception as e:
@@ -54,7 +61,10 @@ class GroupRepository(AbstractRepository[GroupDTO]):
     def remove_join_request(self, group_id: int, student_id: int) -> None:
         self.__find_opened_join_request(group_id, student_id)
         try:
-            stm = delete(student_group).where(student_group.columns["group_id"] == group_id).where(student_group.columns["student_id"] == student_id).where(student_group.columns["approved"] == False)
+            stm = delete(student_group)\
+                .where(student_group.columns['group_id'] == group_id)\
+                .where(student_group.columns['student_id'] == student_id)\
+                .where(student_group.columns['approved'] == False)
             self._session.execute(stm)
             self._session.commit()
         except Exception as e:
@@ -65,10 +75,12 @@ class GroupRepository(AbstractRepository[GroupDTO]):
         result: list[GroupDTO] = []
         match role:
             case Role.MANAGER:
-                result = self._session.query(GroupDTO).filter_by(manager_id=id).order_by(GroupDTO.created_at).all()
+                result = list(self._session.scalars(select(GroupDTO).where(GroupDTO.manager_id == id).order_by(GroupDTO.created_at)).all())
             case Role.STUDENT:
-                stm = select(student_group.columns).where(student_group.columns["student_id"] == id).where(student_group.columns["approved"] == True)
-                join_requests = self._session.execute(stm)
-                groups = list(map(lambda row: row.group_id, join_requests.all()))
-                result = self._session.query(GroupDTO).filter(GroupDTO.id.in_(groups)).order_by(GroupDTO.created_at).all()
+                stm = select(GroupDTO)\
+                    .join(student_group, GroupDTO.id == student_group.columns['group_id'])\
+                    .join(StudentDTO, StudentDTO.id == student_group.columns['student_id'])\
+                    .where(and_(StudentDTO.id == id, student_group.columns['approved'] == True))\
+                    .order_by(student_group.columns['updated_at'])
+                result = list(self._session.scalars(stm).all())
         return result
