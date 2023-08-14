1ab7eac504dd5dd757b057cd7c11467a5eca5233
Danilo Becke <danilobecke@gmail.com>
2023-08-14 10:42:31 -0300

    Add download latest source code tests

--- code/tests/result_test.py
+++ code/tests/result_test.py
@@ -3,7 +3,7 @@ from io import BytesIO
 
 import pytest
 
-from tests.helper import post, get_manager_id_token, create_task_json, create_test_case_json, get_student_id_token, create_join_request_group_id, CONTENT_TYPE_FORM_DATA, get_filepath_of_size, get_new_group_id_code, get_random_name
+from tests.helper import post, get_manager_id_token, create_task_json, create_test_case_json, get_student_id_token, create_join_request_group_id, CONTENT_TYPE_FORM_DATA, get_filepath_of_size, get_new_group_id_code, get_random_name, get
 
 VALID_C_CODE = '''
 #include<stdio.h>
@@ -33,6 +33,7 @@ int main() {
 }
 '''
 
+# pylint: disable=too-many-public-methods
 class TestResult:
     def __set_up_task_id_student_token(self, starts_on: str | None = None, ends_on: str | None = None, max_attempts: int | None = None) -> tuple[str, str]:
         manager_token = get_manager_id_token()[1]
@@ -52,6 +53,14 @@ class TestResult:
         create_test_case_json(manager_token, task_id, closed=True, content_in='5 6', content_out='11')
         create_test_case_json(manager_token, task_id, closed=True, content_in='7 8', content_out='15')
         return (task_id, student_token)
+
+    def __set_up_result_source_code_url_student_token(self, code: str) -> tuple[str, str]:
+        task_id, student_token = self.__set_up_task_id_student_token()
+        payload = {
+            'code': (BytesIO(code.encode('utf-8')), 'code.c')
+        }
+        source_url = post(f'/api/v1/tasks/{task_id}/results', payload, student_token, CONTENT_TYPE_FORM_DATA)[1]['source_url']
+        return (source_url, student_token)
 
     def test_post_result_without_code_should_fail(self) -> None:
         task_id, student_token = self.__set_up_task_id_student_token()
@@ -250,3 +259,65 @@ class TestResult:
         assert open_result['diff'] == 'Execution error:\nError'
         closed_results = result['closed_results']
         assert len(closed_results) == 0
+
+    def test_download_latest_source_code_with_student_should_succeed(self) -> None:
+        source_url, student_token = self.__set_up_result_source_code_url_student_token(VALID_C_CODE)
+
+        response = get(source_url, student_token, decode_as_json=False)
+
+        assert response[0] == 200
+        assert str(response[1]) == VALID_C_CODE
+
+    @pytest.mark.smoke
+    def test_download_latest_source_code_with_multiple_results_should_return_latest(self) -> None:
+        task_id, student_token = self.__set_up_task_id_student_token()
+        payload_first = {
+            'code': (BytesIO(VALID_C_CODE.encode('utf-8')), 'code.c')
+        }
+        source_url = post(f'/api/v1/tasks/{task_id}/results', payload_first, student_token, CONTENT_TYPE_FORM_DATA)[1]['source_url']
+        payload_second = {
+            'code': (BytesIO(INVALID_C_CODE.encode('utf-8')), 'code.c')
+        }
+        post(f'/api/v1/tasks/{task_id}/results', payload_second, student_token, CONTENT_TYPE_FORM_DATA)
+
+        response = get(source_url, student_token, decode_as_json=False)
+
+        assert response[0] == 200
+        assert str(response[1]) == INVALID_C_CODE
+
+    def test_download_latest_source_code_with_manager_should_return_unauthorized(self) -> None:
+        manager_token = get_manager_id_token()[1]
+        source_url = self.__set_up_result_source_code_url_student_token(VALID_C_CODE)[0]
+
+        response = get(source_url, manager_token, decode_as_json=False)
+
+        assert response[0] == 401
+
+    def test_download_latest_source_code_with_another_student_should_return_forbidden(self) -> None:
+        source_url = self.__set_up_result_source_code_url_student_token(VALID_C_CODE)[0]
+        name = get_random_name()
+        student_payload = {
+            'name': name,
+            'email': name + '@mail.com',
+            'password': name + 'password'
+        }
+        another_student_token = post('/api/v1/students', student_payload)[1]['token']
+
+        response = get(source_url, another_student_token, decode_as_json=False)
+
+        assert response[0] == 403
+
+    def test_download_latest_source_code_with_invalid_task_id_should_return_not_found(self) -> None:
+        student_token = get_student_id_token()[1]
+
+        response = get(f'/api/v1/tasks/{999999}/results/latest/code', student_token)
+
+        assert response[0] == 404
+
+    @pytest.mark.smoke
+    def test_download_latest_source_code_without_result_should_return_not_found(self) -> None:
+        task_id, student_token = self.__set_up_task_id_student_token()
+
+        response = get(f'api/v1/tasks/{task_id}/results/latest/code', student_token, decode_as_json=False)
+
+        assert response[0] == 404
