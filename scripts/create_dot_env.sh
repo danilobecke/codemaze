46b8c8d54ffd05882714a59d68d755cb1ba0d7e8
Danilo Becke <danilobecke@gmail.com>
2023-09-01 11:20:17 -0300

    Add memory limit for runners' container

--- scripts/create_dot_env.sh
+++ scripts/create_dot_env.sh
@@ -3,12 +3,6 @@
 ENV=$1
 DOT_ENV=".env"
 
-# custom keys
-ENV_DOT_ENV=".env.$ENV"
-if test -f "$ENV_DOT_ENV"; then
-    cat "$ENV_DOT_ENV" > "$DOT_ENV"
-fi
-
 # postgres password
 POSTGRES_DOT_ENV=".env.postgres.$ENV"
 POSTGRES_PASSWORD=`openssl rand -hex 32`
@@ -21,7 +15,7 @@ echo POSTGRES_PASSWORD=\""$POSTGRES_PASSWORD"\" > .env.postgres
 echo POSTGRES_PASSWORD=\""$POSTGRES_PASSWORD"\" > "$POSTGRES_DOT_ENV"
 
 # JWT key
-echo CODEMAZE_KEY=\"$(openssl rand -base64 32)\" >> "$DOT_ENV"
+echo CODEMAZE_KEY=\"$(openssl rand -base64 32)\" > "$DOT_ENV"
 
 # DB address
 DB_ADDRESS="localhost:5432"
@@ -29,3 +23,13 @@ if [ "$ENV" = "deploy" ]; then
     DB_ADDRESS="postgres:5432"
 fi
 echo CODEMAZE_DB_STRING=\"postgresql://codemaze:"$POSTGRES_PASSWORD"@"$DB_ADDRESS"/codemaze_db\" >> "$DOT_ENV"
+
+# custom keys
+ENV_DOT_ENV=".env.$ENV"
+if test -f "$ENV_DOT_ENV"; then
+    cat "$ENV_DOT_ENV" >> "$DOT_ENV"
+fi
+
+# runner max memory allowed
+MEM_ALLOWED=`cat config.toml | grep max-memory-mb | awk -F '=' '{print $2}'`
+echo RUNNER_MAX_MEM=\""$MEM_ALLOWED"\" >> "$DOT_ENV"
