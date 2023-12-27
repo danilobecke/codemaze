7c84fc1ce2b6f7e88cfbbbcf262adfe540edde7f
Danilo Becke <danilobecke@gmail.com>
2023-12-27 16:20:39 -0300

    Use the correct percentage on reports

--- code/endpoints/models/result_vo.py
+++ code/endpoints/models/result_vo.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from endpoints.models.tcase_result_vo import TCaseResultVO
-from helpers.commons import latest_source_code_download_url
+from helpers.commons import compute_percentage, latest_source_code_download_url
 from repository.dto.result import ResultDTO
 
 class ResultVO:
@@ -24,7 +24,7 @@ class ResultVO:
         vo.attempt_number = attempt_numer
         vo.open_result_percentage = round((dto.correct_open / len_open_tests) * 100, 2)
         vo.closed_result_percentage = round((dto.correct_closed / len_closed_tests) * 100, 2) if len_closed_tests > 0 else None
-        vo.result_percentage = vo.open_result_percentage if vo.closed_result_percentage is None else round((vo.open_result_percentage * len_open_tests + vo.closed_result_percentage * len_closed_tests) / (len_open_tests + len_closed_tests), 2)
+        vo.result_percentage = compute_percentage(vo.open_result_percentage, vo.closed_result_percentage, len_open_tests, len_closed_tests)
         vo.source_url = latest_source_code_download_url(dto.task_id)
         vo.open_results = open_results
         vo.closed_results = closed_results
