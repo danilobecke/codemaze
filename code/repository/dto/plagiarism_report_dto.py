13972f349105b0a7f384f2c2b3013441790f8b4b
Danilo Becke <danilobecke@gmail.com>
2024-02-05 08:43:24 -0300

    Fix PK sequence - PlagiarismReportDTO and TestCaseResultDTO

--- code/repository/dto/plagiarism_report_dto.py
+++ code/repository/dto/plagiarism_report_dto.py
@@ -6,7 +6,7 @@ from repository.base import Base
 class PlagiarismReportDTO(Base):
     __tablename__ = 'plagiarism_report'
 
-    id: Mapped[int] = mapped_column(Integer, Sequence('sq_result_pk'), primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(Integer, Sequence('sq_plagiarism_report_pk'), primary_key=True, autoincrement=True)
     url: Mapped[str]
     language: Mapped[str]
     task_id: Mapped[int] = mapped_column(Integer, ForeignKey('task.id'), nullable=False)
