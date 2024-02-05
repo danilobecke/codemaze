3cdaa0aacd658941490c8c59ed0959b247ad383a
Danilo Becke <danilobecke@gmail.com>
2024-02-05 07:00:04 -0300

    Update user token payload - add intent

--- code/services/jwt_service.py
+++ code/services/jwt_service.py
@@ -1,4 +1,5 @@
 import datetime
+from enum import StrEnum, auto
 from typing import Any
 
 import jwt
@@ -9,26 +10,35 @@ from helpers.exceptions import ServerError, Unauthorized
 ALGORITHM: str = 'HS256'
 
 class JWTService:
+    class Intent(StrEnum):
+        SESSION = auto()
+
     def __init__(self, key:str) -> None:
         self.__key = key
         expiration_minutes = int(Config.get('admin.session-duration'))
         self.__expiration_delta = datetime.timedelta(days=0, hours=0, minutes=expiration_minutes)
 
-    def create_token(self, user_id: int) -> str:
+    def create_token(self, user_id: int, intent: Intent) -> str:
         payload = {
             'exp': datetime.datetime.utcnow() + self.__expiration_delta,
             'iat': datetime.datetime.utcnow(),
-            'sub': user_id,
+            'sub': {
+                'intent': intent,
+                'user_id': user_id,
+            },
         }
         try:
             return jwt.encode(payload, key=self.__key, algorithm=ALGORITHM)
         except Exception as e:
             raise ServerError() from e
 
-    def decode_token(self, token: str) -> int:
+    def decode_token(self, token: str, intent: Intent) -> int:
         try:
             payload: dict[str, Any] = jwt.decode(token, key=self.__key, algorithms=[ALGORITHM])
-            return int(payload['sub'])
+            sub: dict[str, Any] = payload['sub']
+            if str(sub['intent']) != intent:
+                raise Unauthorized()
+            return int(sub['user_id'])
         except jwt.ExpiredSignatureError as e:
             raise Unauthorized() from e
         except jwt.InvalidTokenError as e:
