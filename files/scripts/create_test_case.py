d4c522e0aca490a4e2a5d7d15bf35539fe4ec620
Danilo Becke <danilobecke@gmail.com>
2024-06-25 09:45:50 -0300

    Improve create_test_case script - recover from deadlocks

--- files/scripts/create_test_case.py
+++ files/scripts/create_test_case.py
@@ -9,7 +9,7 @@ parser = ArgumentParser(usage='create_test_case.py \'python test.py\' 1 [-c]')
 parser.add_argument('executable_command')
 parser.add_argument('test_number')
 parser.add_argument('--closed', '-c', help='whether this is a closed test', action='store_true')
-parser.add_argument('--timeout', '-t', default=2, help='execution timeout. Default = 2')
+parser.add_argument('--timeout', '-t', default=0.1, help='execution timeout. Default = 0.1')
 args = parser.parse_args()
 
 # execute
@@ -20,13 +20,17 @@ stdout = ''
 while exit_code != 0:
     with subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
         stdin += input('# ') + linesep
-        stdout, stderr = process.communicate(input=stdin, timeout=args.timeout)
+        try:
+            stdout, stderr = process.communicate(input=stdin, timeout=float(args.timeout))
+            exit_code = process.returncode
+        except subprocess.TimeoutExpired:
+            process.kill()
+            exit_code = -1
         print('--- current input')
         print(stdin, end='')
         print('--- current output')
         print(stdout, end='')
         print('------------------')
-        exit_code = process.returncode
 
 # normalize stdin
 stdin = stdin.replace(linesep, '\n')
